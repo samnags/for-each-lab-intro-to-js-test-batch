c5f5f37d913885e5b79c18abb9cb8a4da22b2e31
Sam Nagourney <sam.nagourney@gmail.com>
2016-07-20 00:47:36 +0000

    Done.

---
+++ forEach.js
@@ -0,0 +1,15 @@
+function iterativeLog(array) {
+  array.forEach((element, index) => {
+    console.log(`${index}: ${element}`)
+  })
+}
+
+function iterate(callback) {
+  var array = ["blah", "blah", "blacksheet"];
+  array.forEach(callback)
+  return array
+}
+
+function doToArray(array, callback) {
+  array.forEach(callback)
+}
